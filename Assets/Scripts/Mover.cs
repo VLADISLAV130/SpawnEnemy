d7d29bff6c887001c4404449d8706be38fb01d2e
VLADISLAV130 <a676202@gmail.com>
2024-03-11 20:52:32 +0500

    2

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,7 +5,16 @@ public class Mover : MonoBehaviour
     [SerializeField] private Vector3 _movementDirection;
 
     private void Update()
+
+
+
+
     {
+
+
+
+
+
         transform.Translate(_movementDirection * Time.deltaTime);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _spawnObject;
+    [SerializeField] private Rigidbody _spawnObject;
     [SerializeField] private Transform[] _spawnPoints;
 
-    private float _timeBetweenSpawns = 2f;
+    private float _delay = 2f;
     private int _minRandomSpawnPoints = 0;
 
     private void Start()
@@ -14,15 +14,21 @@ public class Spawner : MonoBehaviour
        StartCoroutine(Spawn());
     }    
 
+    private void CreateEnemy()
+    {
+        int _direction = Random.Range(0, 360);
+        _spawnObject.transform.position = _spawnPoints[Random.Range(_minRandomSpawnPoints, _spawnPoints.Length)].position;
+
+        var enemy = Instantiate(_spawnObject, _spawnObject.transform.position, Quaternion.Euler(0, 0, _direction));
+    }
+
     private IEnumerator Spawn()
     {
         while (true)
         {
-            int _direction = Random.Range(0, 360);
-            _spawnObject.transform.position = _spawnPoints[Random.Range(_minRandomSpawnPoints, _spawnPoints.Length)].position;
-
-            Instantiate(_spawnObject, _spawnObject.transform.position, Quaternion.Euler(0,0, _direction));            
-            yield return new WaitForSeconds(_timeBetweenSpawns);
+            CreateEnemy(); 
+            
+            yield return new WaitForSeconds(_delay);
         }
     }
 }
